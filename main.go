6f133a2b75e0ef646acd232d228c03ec0e96033c
Danu Akbar <awcodify@gmail.com>
2017-08-25 17:20:01 +0700

    add simple jwt authentication

--- main.go
+++ main.go
@@ -1,24 +1,55 @@
 package main
 
 import (
+	"github.com/appleboy/gin-jwt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/whatdacode/sporta/config"
 	"github.com/whatdacode/sporta/controllers"
 	"github.com/whatdacode/sporta/models"
+	"time"
 )
 
 func main() {
-
-	//Migrate the schema
 	db := config.Connect()
 	db.AutoMigrate(&models.User{})
 
 	router := gin.Default()
 
+	authMiddleware := &jwt.GinJWTMiddleware{
+		Realm:      "OurRealm",
+		Key:        []byte("OurSecretKey"),
+		Timeout:    time.Hour,
+		MaxRefresh: time.Hour,
+		Authenticator: func(userId string, password string, c *gin.Context) (string, bool) {
+			if (userId == "admin" && password == "admin") || (userId == "test" && password == "test") {
+				return userId, true
+			}
+
+			return userId, false
+		},
+		Authorizator: func(userId string, c *gin.Context) bool {
+			if userId == "admin" {
+				return true
+			}
+
+			return false
+		},
+		Unauthorized: func(c *gin.Context, code int, message string) {
+			c.JSON(code, gin.H{
+				"code":    code,
+				"message": message,
+			})
+		},
+		TokenLookup: "header:Authorization",
+		TimeFunc:    time.Now,
+	}
+
 	api := router.Group("/api/v1/")
 	{
+		api.POST("/login", authMiddleware.LoginHandler)
 		users := api.Group("/users")
+		users.Use(authMiddleware.MiddlewareFunc())
 		{
 			users.GET("/", controllers.GetUsers)
 			users.POST("/", controllers.CreateUser)
